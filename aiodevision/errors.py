c401e72c7ab9acc46c2714916f9fcbef5c170bb4
proguy914629 <74696067+proguy914629bot@users.noreply.github.com>
2021-06-15 12:51:03 +0700

    Create errors.py

---
+++ aiodevision/errors.py
@@ -0,0 +1,22 @@
+class UndefinedLibraryError(Exception):
+    pass
+
+
+class TokenRequired(Exception):
+    pass
+
+
+class InvalidImage(Exception):
+    pass
+
+
+class InternalServerError(Exception):
+    pass
+
+
+class InvalidDocumentation(Exception):
+    pass
+
+
+class InvalidData(Exception):
+    pass
