60e8dfa63f84d608b423070b0a356880dc2d0341
proguy914629 <74696067+proguy914629bot@users.noreply.github.com>
2021-06-16 14:52:35 +0700

    Update errors.py

--- aiodevision/errors.py
+++ aiodevision/errors.py
@@ -1,22 +1,32 @@
-class UndefinedLibraryError(Exception):
-    pass
+class AioDevisionException(Exception):
+    """The Base exceptions to all other exceptions."""
+
+class UndefinedLibraryError(AioDevisionException):
+    """An Exception when you defined a undefined lib in RTFS endpoint."""
 
 
-class TokenRequired(Exception):
-    pass
+class TokenRequired(AioDevisionException):
+    """An Exception when you are trying to access an endpoint that requires a token."""
 
 
-class InvalidImage(Exception):
-    pass
+class InvalidImage(AioDevisionException):
+    """An Exception when you provided an invalid image to OCR and/or CDN."""
 
 
-class InternalServerError(Exception):
-    pass
+class InternalServerError(AioDevisionException):
+    """An Exception when an unknown error happens. Go join x and Scream at IAmTomahawkx#1000."""
 
 
-class InvalidDocumentation(Exception):
-    pass
+class InvalidDocumentation(AioDevisionException):
+    """You provided an invalid documentation to the RTFM endpoint."""
 
 
-class InvalidData(Exception):
-    pass
+class InvalidData(AioDevisionException):
+    """You provided an invalid data in a request you made."""
+
+
+class NotFound(AioDevisionException):
+    """The URL provided was Not Found."""
+
+class MaxRetryReached(AioDevisionException):
+    """Max retries reached for a request. This can be changed using Client.retry."""
