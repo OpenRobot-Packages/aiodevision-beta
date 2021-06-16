745c982db14e59bc52cf4360135c43c3c86eda34
proguy914629 <74696067+proguy914629bot@users.noreply.github.com>
2021-06-16 16:00:32 +0700

    Update client.py

--- aiodevision/client.py
+++ aiodevision/client.py
@@ -9,6 +9,7 @@ from .errors import *
 from .baseclasses import *
 from .enums import *
 from .http import HTTPClient
+from .games import Games
 
 class Client:
     def __init__(self, token: typing.Optional[str] = None, *, url: str = "https://idevision.net/", retry: int = 5) -> None:
@@ -30,30 +31,37 @@ class Client:
         self,
         *,
         query: typing.Optional[str] = None,
-        library: str,
-        format: typing.Optional[str] = 'links',
+        library: typing.Union[LibraryEnum, str] = None,
+        format: typing.Union[RTFSFormat, str] = RTFSFormat.links,
     ) -> RTFS:
         """Queries a python module's github link for the query you give and gives relevant github links to what you searched for.
 
         :param query: The actual query
         :type query: typing.Optional[str]
         :param library: The library to find source for
-        :type library: str
+        :type library: typing.Union[LibraryEnum, str]
         :param format: the format you want the data to be returned as. can be either 'links', which returns the github links, and 'source', which returns the code it self, defaults to 'links'
-        :type format: typing.Optional[str], optional
+        :type format: typing.Union[RTFSFormat, str], optional
         :raises UndefinedLibraryError: This library cannot be queried.
         :raises InternalServerError: An Internal Server Error occured while requesting the API.
         :return: A RTFS Class
         :rtype: RTFS
         """
+        if not isinstance(library, (LibraryEnum, str)):
+            raise UndefinedLibraryError(
+                'The Library specficied cannot by queried. Please provide a library from the following list: twitchio, wavelink, discord.py, or aiohttp.'
+            )
+        library = str(library).lower()
         if library.lower() not in [
             'twitchio',
             'wavelink',
             'aiohttp',
             'discord.py',
             'discord.py-2',
+            'discord.py2',
             'dpy',
             'dpy2',
+            'dpy-2'
         ]:
             raise UndefinedLibraryError(
                 'The Library specficied cannot by queried. Please provide a library from the following list: twitchio, wavelink, discord.py, or aiohttp.'
@@ -101,80 +109,15 @@ class Client:
             data = await resp.json()
         return RTFM(data['nodes'], float(data['query_time']))
     
-    async def chess(self, data: typing.Union[ChessData, dict]):
-        if not self.token:
-            raise TokenRequired('A Token is required to access this endpoint')
-
-        if not isinstance(data, (ChessData, dict)):
-            raise InvalidData(f'Expected ChessData or dict but got {data.__class__.__name__} instead')
-
-        payload = {}
-
-        if isinstance(data, ChessData):
-            payload = data.to_payload()
-        elif isinstance(data, dict):
-            payload = data
-
-        if 'white-theme' not in payload:
-            raise InvalidData("Missing 'white-theme' key.")
-        if 'black-theme' not in payload:
-            raise InvalidData("Missing 'black-theme' key.")
-        if 'board-theme' not in payload:
-            raise InvalidData("Missing 'board-theme' key.")
-
-        async with self.session.post('https://idevision.net/api/games/chess', data=payload) as resp:
-            return await resp.json()
-
-    async def chess_render(self, data: typing.Union[ChessRender, dict], arrow: typing.Union[typing.Any, None] = None):
-        if not self.token:
-            raise TokenRequired('A Token is required to access this endpoint')
-
-        if not isinstance(data, (ChessRender, dict)):
-            raise InvalidData(f'Expected ChessRender or dict but got {data.__class__.__name__} instead')
-
-        payload = {}
-
-        if isinstance(data, ChessRender):
-            payload = data.to_payload()
-        elif isinstance(data, dict):
-            payload = data
-
-        async with self.session.post('https://idevision.net/api/games/chess/render', data=payload) as resp:
-            return BytesIO(await resp.read())
-
-    async def chess_turn(self, data: typing.Union[ChessTurn, dict]):
-        if not self.token:
-            raise TokenRequired('A Token is required to access this endpoint')
-
-        if not isinstance(data, (ChessTurn, dict)):
-            raise InvalidData(f'Expected ChessTurn or dict but got {data.__class__.__name__} instead')
-
-        payload = {}
-
-        if isinstance(data, ChessTurn):
-            payload = data.to_payload()
-        elif isinstance(data, dict):
-            payload = data
-
-        async with self.session.post('https://idevision.net/api/games/chess/turn', data=payload) as resp:
-            return await resp.json()
-
-    async def chess_transcript(self, data: typing.Union[ChessTranscript, dict]):
-        if not self.token:
-            raise TokenRequired('A Token is required to access this endpoint')
-
-        if not isinstance(data, (ChessTranscript, dict)):
-            raise InvalidData(f'Expected ChessTranscript or dict but got {data.__class__.__name__} instead')
-
-        payload = {}
-
-        if isinstance(data, ChessTranscript):
-            payload = data.to_payload()
-        elif isinstance(data, dict):
-            payload = data
-
-        async with self.session.post('https://idevision.net/api/games/chess/transcript', data=payload) as resp:
-            return await resp.text()
+    @property
+    def _reload_http(self) -> HTTPClient:
+        http_client = HTTPClient(self)
+        self.http = http_client
+        return http_client
+    
+    @property
+    def games(self):
+        return Games(self)
 
     async def ocr(self, image: BytesIO) -> str:
         if not self.token:
